3df4aa9828fd0f16b7f7280f98892ba4cdb7aaed
Luan <luann0233@gmail.com>
2023-04-12 21:59:08 -0300

    feat: começando a implementar tabela de listagem

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,64 +1,44 @@
 <template>
-  <v-container class="bg-surface-variant">
-    <v-row>
-      <v-col>
-        <SelectStatus v-model="status" />
-      </v-col>
+  <div>
 
-      <v-col>
-        <SelectSexo v-model="sexo" />
-      </v-col>
+    <v-data-table
+      :loading="loading"
+      class="elevation-1"
+      sort-by="id"
+      :headers="headers"
+      :items="users"
+      :server-items-length="totalItensServer"
+    >
+      <template #[`item.gender`]="{ item }">
+        <v-chip
+          :color="getColorGender(item.gender)"
+          dark
+        >
+          {{ getLabelGender(item.gender) }}
+        </v-chip>
+      </template>
 
-      <v-col>
-        <v-text-field
-          v-model="name"
-          label="Nome"
-          append-icon="mdi-account-group"
-          :rules="rules"
-          hide-details="auto"
-        />
-      </v-col>
+      <template #[`item.actions`]="{ item }">
+        <v-icon
+          small
+          class="mr-2"
+          @click="editItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          small
+          @click="deleteItem(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
 
-      <v-col>
-        <v-text-field
-          v-model="email"
-          label="Email"
-          append-icon="mdi-at"
-          hide-details="auto"
-        />
-      </v-col>
-
-      <v-col align-self="center" class="text-align-end">
-        <v-btn prepend-icon="mdi-magnify">
-          Aplicar
-        </v-btn>
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <ul>
-        <li v-for="(user, index) in users" :key="index">
-          <p>
-            <b>Nome: </b> {{ user.name }}
-          </p>
-
-          <p>
-            <b>Email: </b> {{ user.email }}
-          </p>
-
-          <p>
-            <b>Genero: </b> {{ user.gender }}
-          </p>
-
-          <p>
-            <b>Status: </b> {{ user.status }}
-          </p>
-
-          <hr>
-        </li>
-      </ul>
-    </v-row>
-  </v-container>
+      <template #no-data>
+        Sem Dados
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
@@ -74,11 +54,46 @@ export default {
 
   data () {
     return {
+      loading: false,
+      headers: [
+        {
+          text: 'ID',
+          align: 'start',
+          sortable: true,
+          value: 'id'
+        },
+        {
+          text: 'Nome',
+          align: 'start',
+          sortable: true,
+          value: 'name'
+        },
+        {
+          text: 'E-mail',
+          align: 'start',
+          sortable: true,
+          value: 'email'
+        },
+        {
+          text: 'Gênero',
+          align: 'start',
+          sortable: false,
+          value: 'gender'
+        },
+        {
+          text: 'Status',
+          align: 'start',
+          sortable: false,
+          value: 'status'
+        },
+        {
+          text: 'Actions',
+          value: 'actions',
+          sortable: false
+        }
+      ],
       users: [],
-      status: '',
-      sexo: '',
-      nome: '',
-      email: ''
+      totalItensServer: 0
     }
   },
 
@@ -87,11 +102,31 @@ export default {
   },
 
   methods: {
-    async getUsers () {
-      await this.$axios.$get('/users').then((res) => {
-        this.users = res.data
+    async getUsers (config) {
+      this.loading = true
+      await this.$get('/users', config).then((res) => {
+        this.users = res.data.data
+        this.totalItensServer = res.data.meta.pagination.total
+      }).finally(() => {
+        this.loading = false
       })
+    },
+
+    getColorGender (gender) {
+      if (gender === 'male') {
+        return 'blue'
+      } else {
+        return 'pink'
+      }
+    },
+
+    getLabelGender (gender) {
+      if (gender === 'male') {
+        return 'Masculino'
+      } else {
+        return 'Feminino'
     }
+    },
   }
 }
 </script>
@@ -100,4 +135,5 @@ export default {
 .text-align-end {
   text-align: end;
 }
-</style>
+}
+</script>
