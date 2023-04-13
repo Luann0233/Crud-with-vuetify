06eaced75ede94c6d1deec9cf7f31785800d7d26
Luan <luann0233@gmail.com>
2023-04-12 21:20:16 -0300

    feat: criando component FiltrosTabela

---
+++ components/FiltrosTabela.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-row justify="space-around" class="mb-2">
+    <v-col :cols="6">
+      <SelectStatus v-model="status" />
+    </v-col>
+
+    <v-col :cols="6">
+      <SelectSexo v-model="gender" />
+    </v-col>
+
+    <v-col :cols="6">
+      <v-text-field
+        v-model="name"
+        label="Nome"
+        append-icon="mdi-account-group"
+        hide-details="auto"
+      />
+    </v-col>
+
+    <v-col :cols="6">
+      <v-text-field
+        v-model="email"
+        label="Email"
+        append-icon="mdi-at"
+        hide-details="auto"
+      />
+    </v-col>
+
+    <v-col
+      align-self="center"
+      class="text-align-end"
+      :cols="6"
+    >
+      <v-btn
+        prepend-icon="mdi-magnify"
+        color="primary"
+        @click="setFilters()"
+      >
+        Aplicar Filtros
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'FiltrosTabela',
+
+  data () {
+    return {
+      status: '',
+      gender: '',
+      name: '',
+      email: ''
+    }
+  },
+
+  methods: {
+    setFilters () {
+      const filters = {
+        status: this.status,
+        gender: this.gender,
+        name: this.name,
+        email: this.email
+      }
+
+      this.$emit('applyFilters', filters)
+    }
+  }
+}
+</script>
